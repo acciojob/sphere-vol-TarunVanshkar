52e8c70a7fc20353661a4d8678fa580a65b11188
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-16 18:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function volume_sphere() {
     //V = 4⁄3πr³
 
     let volume = (4/3)*Math.PI*Math.pow(Number(radius), 3);
-    document.getElementById("volume").value=volume;
+    document.getElementById("volume").value=volume.toFixed(4);
 } 
 
 // window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
